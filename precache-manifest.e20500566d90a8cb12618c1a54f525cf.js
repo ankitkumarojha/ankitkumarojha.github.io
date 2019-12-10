22e3c6d926609917df2ddddeb090fd68e5340115
the-observer-man <ankitkumarojha2@gmail.com>
2019-12-10 23:36:22 +0530

    Add Constraint Layout As Build

---
+++ precache-manifest.e20500566d90a8cb12618c1a54f525cf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1cb13bd4cfd6fee4948acb6de072f3d9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "991109a97a19d398f2b4",
+    "url": "/static/css/main.62837f37.chunk.css"
+  },
+  {
+    "revision": "5bd6c9cc418f384267ec",
+    "url": "/static/js/2.9c2a2465.chunk.js"
+  },
+  {
+    "revision": "991109a97a19d398f2b4",
+    "url": "/static/js/main.e37d9c63.chunk.js"
+  },
+  {
+    "revision": "aa4a0181184f3f166f37",
+    "url": "/static/js/runtime-main.84c5e989.js"
+  }
+]);
